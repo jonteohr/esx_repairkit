6c2fc5457e54c4213bb0e3705e4a83fec0b853af
Jonathan Öhrström <tacco_9@hotmail.com>
2018-03-12 15:58:21 +0100

    initial

---
+++ locales/sv.lua
@@ -0,0 +1,7 @@
+Locales ['sv'] = {
+	['used_kit']					= 'du använde ~y~x1 ~b~Reparationskit',
+	['must_be_outside']				= 'du måste vara utanför fordonet!',
+	['no_vehicle_nearby']			= 'det finns inget ~r~fordon ~w~i närheten',
+	['finished_repair']				= '~g~du reparerade fordonet!',
+	['aborted_repair']				= 'du ~r~avbröt ~w~reparationen',
+}
---
+++ server/main.lua
@@ -0,0 +1,21 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+-- Make the kit usable!
+ESX.RegisterUsableItem('repairkit', function(source)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+
+	TriggerClientEvent('esx:showNotification', _source, _U('used_kit'))
+	TriggerClientEvent('esx_repairkit:onUse', _source)
+end)
+
+RegisterNetEvent('esx_repairkit:removeKit')
+AddEventHandler('esx_repairkit:removeKit', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+
+	if Config.InfiniteRepairs then
+		xPlayer.removeInventoryItem('repairkit', 1)
+	end
+end)
