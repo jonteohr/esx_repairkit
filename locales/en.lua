ac982367c152eaaa8fa79d5a9e28542bcf7d45ff
Jonathan Öhrström <tacco_9@hotmail.com>
2018-03-12 16:21:56 +0100

    added english locale

---
+++ locales/en.lua
@@ -0,0 +1,7 @@
+Locales ['en'] = {
+	['used_kit']					= 'you used ~y~x1 ~b~Repairkit',
+	['must_be_outside']				= 'you must be outside of the vehicle!',
+	['no_vehicle_nearby']			= 'there is no ~r~vehicle ~w~nearby',
+	['finished_repair']				= '~g~you repaired the vehicle!',
+	['aborted_repair']				= 'you ~r~aborted ~w~the repairs',
+}
