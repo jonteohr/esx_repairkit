ee76fc1a6f1a5671474b8d5491062d51c7f4f831
Jonathan Öhrström <tacco_9@hotmail.com>
2018-03-13 07:52:43 +0100

    locale fix

--- locales/en.lua
+++ locales/en.lua
@@ -3,5 +3,6 @@ Locales ['en'] = {
 	['must_be_outside']				= 'you must be outside of the vehicle!',
 	['no_vehicle_nearby']			= 'there is no ~r~vehicle ~w~nearby',
 	['finished_repair']				= '~g~you repaired the vehicle!',
+	['abort_hint']					= 'press ~INPUT_VEH_DUCK~ to cancel',
 	['aborted_repair']				= 'you ~r~aborted ~w~the repairs',
 }
