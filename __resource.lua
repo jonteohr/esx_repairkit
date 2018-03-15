fe9f19d8710dfec0c9a3d5b071d0978f5f2d8c51
Jonathan Öhrström <tacco_9@hotmail.com>
2018-03-15 09:02:45 +0100

    1.0.2

--- __resource.lua
+++ __resource.lua
@@ -1,11 +1,12 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Repairkit'
-version '1.0.1'
+version '1.0.2'
 
 client_scripts {
 	'@es_extended/locale.lua',
 	'client/main.lua',
+	'locales/en.lua',
 	'locales/sv.lua',
 	'config.lua'
 }
@@ -13,6 +14,7 @@ client_scripts {
 server_scripts {
 	'@es_extended/locale.lua',
 	'server/main.lua',
+	'locales/en.lua',
 	'locales/sv.lua',
 	'config.lua'
 }
