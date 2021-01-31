c0b2a08d221f705c300c97531bdd5726dc1f2bf1
metl-play <68977058+metl-play@users.noreply.github.com>
2021-01-31 12:32:54 +0100

    Minor changes to the locales. (#5)
    * Create de.lua
    
    Added Language 'de'.
    
    * Update de.lua
    
    * Update en.lua
    
    Changed to make it more readable
    
    * Update sv.lua
    
    Made it more readable. No actual change in the translation!
    
    * Update en.lua
    
    Made it more readable. No actual change in the translation!
    
    * Update de.lua
    
    Made it more readable. No actual change in the translation!
    
    * added 'locales/de.lua'
    
    * Version 1.0.3
    
    added 'locales/de.lua'
    
    * version 1.0.3
    
    * 1.0.3
    
    * 1.0.3

--- __resource.lua
+++ __resource.lua
@@ -1,13 +1,14 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Repairkit'
-version '1.0.2'
+version '1.0.3'
 
 client_scripts {
 	'@es_extended/locale.lua',
 	'client/main.lua',
 	'locales/en.lua',
 	'locales/sv.lua',
+	'locales/de.lua',
 	'config.lua'
 }
 
@@ -16,5 +17,6 @@ server_scripts {
 	'server/main.lua',
 	'locales/en.lua',
 	'locales/sv.lua',
+	'locales/de.lua',
 	'config.lua'
 }
---
+++ locales/de.lua
@@ -0,0 +1,8 @@
+Locales ['de'] = {
+	['used_kit']			= 'du hast ~y~x1 ~b~Reperaturkit benutzt.',
+	['must_be_outside']		= 'Sie müssen sich außerhalb des Fahrzeugs befinden!',
+	['no_vehicle_nearby']		= 'kein ~r~Fahrzeug ~w~in der Nähe',
+	['finished_repair']		= '~g~Sie haben das Fahrzeug repariert!',
+	['abort_hint']			= 'Drücken Sie ~INPUT_VEH_DUCK~ um den Vorgang abzubrechen',
+	['aborted_repair']		= 'Sie haben die Reparatur ~r~abgebrochen ~w~.',
+}
