6bd93a9c1a850cd58085789be2fe1d6130f72618
autoflowerkush <100061006+autoflowerkush@users.noreply.github.com>
2023-07-17 01:33:47 -0400

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,8 @@
+Locales ['en'] = {
+	['used_kit']			= 'you used ~y~x1 ~b~Repairkit',
+	['must_be_outside']		= 'you must be outside of the vehicle!',
+	['no_vehicle_nearby']		= 'there is no ~r~vehicle ~w~nearby',
+	['finished_repair']		= '~g~you repaired the vehicle!',
+	['abort_hint']			= 'press ~INPUT_VEH_DUCK~ to cancel',
+	['aborted_repair']		= 'you ~r~aborted ~w~the repairs',
+}
---
+++ server/main.lua
@@ -0,0 +1,25 @@
+
+-- Make the kit usable!
+ESX.RegisterUsableItem('repairkit', function(source)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	
+	if Config.AllowMecano then
+		TriggerClientEvent('esx_repairkit:onUse', _source)
+	else
+		if xPlayer.job.name ~= 'mecano' then
+			TriggerClientEvent('esx_repairkit:onUse', _source)
+		end
+	end
+end)
+
+RegisterNetEvent('esx_repairkit:removeKit')
+AddEventHandler('esx_repairkit:removeKit', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+
+	if not Config.InfiniteRepairs then
+		xPlayer.removeInventoryItem('repairkit', 1)
+		TriggerClientEvent('esx:showNotification', _source, _U('used_kit'))
+	end
+end)
